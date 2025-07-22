08cd7b40837a97d0990799d8b48b39d05898f831
Yashraj Jaiswal <yashraj6370@gmail.com>
2025-07-22 18:52:26 +0530

    initial commit

---
+++ apps/web/src/utils/orpc.ts
@@ -0,0 +1,36 @@
+import { createORPCClient } from "@orpc/client";
+import { RPCLink } from "@orpc/client/fetch";
+import { createTanstackQueryUtils } from "@orpc/tanstack-query";
+import { QueryCache, QueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
+import type { appRouter } from "../../../server/src/routers/index";
+import type { RouterClient } from "@orpc/server";
+
+export const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: (error) => {
+      toast.error(`Error: ${error.message}`, {
+        action: {
+          label: "retry",
+          onClick: () => {
+            queryClient.invalidateQueries();
+          },
+        },
+      });
+    },
+  }),
+});
+
+export const link = new RPCLink({
+  url: `${import.meta.env.VITE_SERVER_URL}/rpc`,
+  fetch(url, options) {
+    return fetch(url, {
+      ...options,
+      credentials: "include",
+    });
+  },
+});
+
+export const client: RouterClient<typeof appRouter> = createORPCClient(link)
+
+export const orpc = createTanstackQueryUtils(client)
