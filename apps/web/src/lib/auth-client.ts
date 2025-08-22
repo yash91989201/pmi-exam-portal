ab68926c625322350f6d178042b7abbd76f7bb32
Yashraj Jaiswal <yashraj6370@gmail.com>
2025-08-23 00:18:55 +0530

    use constant value temporarily

--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -2,7 +2,7 @@ import { adminClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-	baseURL: import.meta.env.VITE_SERVER_URL,
+	baseURL: "https://api.exam.dev.pmiindia.org",
 	plugins: [adminClient()],
 });
 
