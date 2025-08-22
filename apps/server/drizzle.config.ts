9f36aa8344c37c0410540aa677fcda321370fc3b
Yashraj Jaiswal <yashraj6370@gmail.com>
2025-08-23 00:29:14 +0530

    docker

--- apps/server/drizzle.config.ts
+++ apps/server/drizzle.config.ts
@@ -1,5 +1,19 @@
+import { createEnv } from "@t3-oss/env-core";
 import { defineConfig } from "drizzle-kit";
-import { env } from "./src/env";
+import z from "zod";
+
+export const env = createEnv({
+	server: {
+		DATABASE_URL: z.url(),
+	},
+
+	runtimeEnv: {
+		DATABASE_URL: process.env.DATABASE_URL,
+	},
+
+	skipValidation: !!process.env.SKIP_ENV_VALIDATION,
+	emptyStringAsUndefined: true,
+});
 
 export default defineConfig({
 	schema: "./src/db/schema",
