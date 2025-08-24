1e06ba0f65502730b5ac01ab5cb3861428b4d176
Yashraj Jaiswal <yashraj6370@gmail.com>
2025-08-24 19:32:52 +0530

    Enable preview configuration with allowedHosts

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -13,9 +13,9 @@ export default defineConfig({
 			"@server-types": path.resolve(__dirname, "../server/src/lib/types"),
 		},
 	},
-	// preview: {
-	// 	allowedHosts: import.meta.env.VITE_ALLOWED_HOSTS
-	// 		? import.meta.env.VITE_ALLOWED_HOSTS.split(",")
-	// 		: [],
-	// },
+	preview: {
+	 	allowedHosts: import.meta.env.VITE_ALLOWED_HOSTS
+	 		? import.meta.env.VITE_ALLOWED_HOSTS.split(",")
+	 		: [],
+	 },
 });
