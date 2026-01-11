6bfa8268b255a69388ac96d7ab6b798a5b27e04b
Yashraj Jaiswal <yashraj6370@gmail.com>
2026-01-11 12:38:18 +0530

    update header image size

--- apps/web/src/components/user/shared/header.tsx
+++ apps/web/src/components/user/shared/header.tsx
@@ -6,7 +6,7 @@ export function Header() {
 		<header className="border-border border-b bg-background">
 			<div className="container mx-auto flex h-18 items-center justify-between p-3 md:p-0">
 				<Link to="/" className="flex items-center gap-6">
-					<img src="/pmi_logo.webp" alt="PMI Logo" className="h-30" />
+					<img src="/pmi_logo.webp" alt="PMI Logo" className="h-24" />
 				</Link>
 				<div className="flex items-center gap-4">
 					<Button variant="ghost" asChild>
